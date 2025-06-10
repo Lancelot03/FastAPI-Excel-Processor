eb5e2d3920baf2ccb6fa38695352274648416cf9
Harsh <90236987+Lancelot03@users.noreply.github.com>
2025-06-10 11:55:51 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+from typing import List, Union
 
+class ListTablesResponse(BaseModel):
+    tables: List[str]
+
+class TableDetailsResponse(BaseModel):
+    table_name: str
+    row_names: List[str]
+
+class RowSumResponse(BaseModel):
+    table_name: str
+    row_name: str
+    sum: Union[int, float]
